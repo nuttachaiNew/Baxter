1d357fe1506a273f962c14f67cd6b75b873cdfd3
Jay <korrakote.j@gmail.com>
2018-07-11 14:11:08 +0700

    add find user by username

--- src/main/java/com/demo/spring/SpringBootOAuth2/service/UserService.java
+++ src/main/java/com/demo/spring/SpringBootOAuth2/service/UserService.java
@@ -1,5 +1,7 @@
 package com.demo.spring.SpringBootOAuth2.service;
 
+import com.demo.spring.SpringBootOAuth2.domain.app.User;
+
 import java.util.Map;
 
 public interface UserService {
@@ -7,4 +9,5 @@ public interface UserService {
     Map<String,String> saveUser(String json);
     Map<String,String> updateUser(String json);
     Map<String,String> deleteUser(String json);
+    User findUserByUsername(String userName);
 }
