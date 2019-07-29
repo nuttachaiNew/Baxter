12bf5668222c370b0fe8ea4ef25d68da0142885c
devJay <korrakote.j@gmail.com>
2019-07-29 17:03:15 +0700

    Add report cliria

--- src/main/java/com/demo/spring/SpringBootOAuth2/util/ConstantVariableUtil.java
+++ src/main/java/com/demo/spring/SpringBootOAuth2/util/ConstantVariableUtil.java
@@ -8,5 +8,6 @@ public class ConstantVariableUtil {
 
     public static final String PARAMETER_PATH_FILE_UPLOAD = "100";
     public static final String PARAMETER_DETAIL_PATH_FILE_UPLOAD = "100";
+    public static final String PATH_IMAGE_FOR_JASPER = "C:\\Work\\Baxter\\Baxter\\src\\main\\resources\\image\\ClariaLogo.jpg";
 
 }
